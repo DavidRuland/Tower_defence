7490bea9f951754671175d585b2df337a56cac23
DavidRuland <david.ruland1@gmail.com>
2015-10-15 20:50:29 +0200

    commit2

--- android/src/com/mygdx/game/android/AndroidLauncher.java
+++ android/src/com/mygdx/game/android/AndroidLauncher.java
@@ -2,15 +2,23 @@ package com.mygdx.game.android;
 
 import android.os.Bundle;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
 import com.mygdx.game.MyGdxGame;
 
 public class AndroidLauncher extends AndroidApplication {
+	assetFolder aF;
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new MyGdxGame(), config);
+		//AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		AndroidApplicationConfiguration config= new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useCompass = false;
+        aF=new assetFolder();
+		initialize(aF, config);
+
 	}
 }
